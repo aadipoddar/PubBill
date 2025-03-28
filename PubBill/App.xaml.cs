4eab2a035df1fa356b2e565bc7004059e32c08c9
Aadi Poddar <aadipoddarmail@gmail.com>
2025-03-28 09:09:20 +0530

    Setup Project

---
+++ PubBill/App.xaml.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows;
+
+namespace PubBill;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
---
+++ PubBill/SalesPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace PubBill;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class SalesPage : Window
+{
+	public SalesPage()
+	{
+		InitializeComponent();
+	}
+}
